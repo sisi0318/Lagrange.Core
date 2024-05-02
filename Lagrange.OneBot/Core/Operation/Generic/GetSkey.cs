7dd25192ef7fbdc642d09df183d07283f11634c7
思思 <71646091+sisi0318@users.noreply.github.com>
2024-05-02 15:01:12 +0800

    Update GetSkey.cs

--- Lagrange.OneBot/Core/Operation/Generic/GetSkey.cs
+++ Lagrange.OneBot/Core/Operation/Generic/GetSkey.cs
@@ -1,14 +1,16 @@
 using System.Text.Json.Nodes;
 using Lagrange.Core;
 using Lagrange.OneBot.Core.Notify;
+using Lagrange.Core.Common.Interface.Api;
 using Lagrange.OneBot.Core.Entity.Action;
 
 namespace Lagrange.OneBot.Core.Operation.Generic
+
 {
     [Operation("get_skey")]
     public class GetSkey : IOperation
     {
-        public async Task<OneBotResult> Getskeyy(BotContext context, JsonNode? payload)
+        public async Task<OneBotResult> Getskeyy(BotContext context)
         {
             var cookies = await context.GetSKey();
             return new OneBotResult(new JsonObject { { "skey", cookies } }, 0, "ok");
