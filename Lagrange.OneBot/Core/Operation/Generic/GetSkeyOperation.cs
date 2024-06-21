f7f3dfc969539a3fe5fa518854dc5ea67b4e1663
sisi0318 <gamemode12345@163.com>
2024-06-21 21:28:32 +0800

    ？

--- Lagrange.OneBot/Core/Operation/Generic/GetSkeyOperation.cs
+++ Lagrange.OneBot/Core/Operation/Generic/GetSkeyOperation.cs
@@ -12,7 +12,7 @@ public class GetSkeyOperation (TicketService ticket) : IOperation
     public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload)
     {
             string skey = await ticket.GetSKey();
-            skey ??="";
+            skey ?? = "";
             return new OneBotResult(new JsonObject { { "skey", skey } }, 0, "ok");
     }
 }
